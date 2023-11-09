aa5144e7be388cdd33258bc042deaf42d439806e
terrobladediao <terrobladediao@gmail.com>
2023-11-09 15:13:18 +1100

    make some route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import Header from './components/Header.js';
-import MainContent from './components/MainContent.js';
-import Footer from './components/Footer.js';
 import './App.css'; // Make sure to include your global styles as well
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
+import MainPage from './pages/MainPage.js'
+import NewsPage from './pages/NewsPage.js'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+    <main>
+    <Router>
+
+      <Container>
+        <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/news" element={<NewsPage />} />
+        </Routes>
+      </Container>
+
+    </Router>
+  </main>
   );
 }
 
---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header.js';
+import MainContent from '../components/MainContent.js';
+import Footer from '../components/Footer.js';
+
+const MainPage = () => {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+export default MainPage;
