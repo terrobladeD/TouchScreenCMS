036cdc13f177562b19e74d303a25db3ffd68ff8c
terrobladediao <terrobladediao@gmail.com>
2023-12-01 17:32:45 +1100

    1

--- frontend/src/components/NewsContent.js
+++ frontend/src/components/NewsContent.js
@@ -1,5 +1,4 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Row, Col } from 'react-bootstrap';
 import AppContext from '../context/AppContext.js';
 
 const NewsContent = () => {
@@ -8,6 +7,7 @@ const NewsContent = () => {
 
     useEffect(() => {
         const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+        console.log(connection);
         if (connection) {
             if (connection.downlink < 1.5) { // lower 1.5mbps will be considered low
                 setIsSlowConnection(true);
@@ -16,21 +16,21 @@ const NewsContent = () => {
     }, []);
 
     return (
-        <div className='main-content' style={{ overflow: 'scroll' }}>
+        <div className='main-content news-list' style={{ overflowY: 'scroll' }}>
             {newsData ? newsData.map((newsItem, index) => (
-                <Row key={index} style={{ width: '100%', height: '25vw', borderBottom: '0.1rem solid white', padding: '1rem', color: 'white' }}>
-                    <Col md={7}>
+                <div key={index} style={{ display:"flex", width: '100%', height: '15rem', borderBottom: '0.1rem solid white', padding: '1rem', color: 'white' }}>
+                    <div style={{width:'70%',padding:'0 4rem 0 0'}}>
                         <h2 style={{ margin: '0 0 2rem 0' }}>{newsItem.header}</h2>
                         <p style={{ margin: '0' }}>{newsItem.text}</p>
-                    </Col>
-                    <Col md={5}>
+                    </div>
+                    <div style={{width:'30%'}}>
                         {isSlowConnection ? (
                             <div style={{ width: '100%', maxHeight: '100%', backgroundColor: '#ccc' }}>Image not loaded due to slow connection</div>
                         ) : (
                             <img src={newsItem.img} alt="News" style={{ width: '100%', maxHeight: '100%', objectFit: 'contain' }} />
                         )}
-                    </Col>
-                </Row>
+                    </div>
+                </div>
             )) : <p>Loading news...</p>}
         </div>
     )
